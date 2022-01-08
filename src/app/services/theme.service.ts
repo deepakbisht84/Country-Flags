970aa3673df931b4b6dca1e4159f8e2f01aa468d
Tannu Bansal <tbansal@codeblock.co.in>
2022-01-08 13:45:19 +0530

    first-commit

---
+++ src/app/services/theme.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+export enum Theme {
+  light = 'light',
+  dark = 'dark',
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ThemeService {
+  private mode: BehaviorSubject<Theme> = new BehaviorSubject(Theme.light);
+  constructor() {}
+
+  get mode$() {
+    return this.mode.asObservable();
+  }
+
+  toggleMode() {
+    if (this.mode.value === Theme.dark) {
+      this.mode.next(Theme.light);
+    } else {
+      this.mode.next(Theme.dark);
+    }
+  }
+}
