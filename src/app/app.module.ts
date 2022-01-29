20208c1510d26bb5ea5fb36bebdac636c6d2ef6b
Deepak Bisht <deepakbisht842@gmail.com>
2022-01-29 13:47:46 +0530

    Final Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DetailComponent } from './pages/detail/detail.component';
+import { CountryCardComponent } from './components/country-card/country-card.component';
+import { DropdownComponent } from './components/dropdown/dropdown.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    DetailComponent,
+    CountryCardComponent,
+    DropdownComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/country-card/country-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Country } from 'src/app/types/api';
+
+@Component({
+  selector: 'app-country-card',
+  templateUrl: './country-card.component.html',
+  styleUrls: ['./country-card.component.scss'],
+})
+export class CountryCardComponent {
+  @Input()
+  country: Country;
+
+  constructor() {}
+}
